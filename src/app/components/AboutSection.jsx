d18a9b556abafd1d8f157121d6fe2aa625302b47
Vitor Marinho <vitormarinhoh@gmail.com>
2024-11-28 13:24:55 -0300

    first commit

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,100 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node.js</li>
+        <li>Next.JS</li>
+        <li>React</li>
+        <li>Typescript</li>
+        <li>SQLServer</li>
+        <li>JavaScript</li>
+        <li>SQL</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Aprendiz/Aluno RocketSeat</li>
+        <li>Infinity Scholl</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>NLW Pocket: Javascript - Programação Iniciante</li>
+        <li>Google Professional Cloud Developer</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+      <Image src="/images/about-image.png" width={500} height={500} alt="" />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">Sobre mim</h2>
+          <p className="text-base lg:text-lg">
+            Sou um desenvolvedor web front-end com paixão por criar sistemas, webformns,
+            web interativos e responsivos. Tenho experiência trabalhando com
+            JavaScript, React, Next.JS, Node.js, HTML, CSS e Git. Sou um aprendiz rápido e estou 
+            sempre procurando expandir meu conhecimento e conjunto de habilidades. Sou um jogador
+            de equipe e estou animado para trabalhar com outros para criar
+            soluções e projetos incríveis.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Habilidades{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Educação{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certificados{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
